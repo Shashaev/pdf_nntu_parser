b1d44a4e4443a3143aa9b26d4ebd00c109ad5a9a
Shashaev <shashaevkirill@gmail.com>
2025-08-02 11:08:51 +0300

    update __init__.py

--- src/parsing_app/events/web_pages/__init__.py
+++ src/parsing_app/events/web_pages/__init__.py
@@ -1,4 +1,4 @@
-import events.pages.education_index
-import events.pages.edu_prog_pdf
-import events.pages.rpds_index
-import events.pages.rpd_pdf
+import events.web_pages.education_index
+import events.web_pages.edu_prog_pdf
+import events.web_pages.rpds_index
+import events.web_pages.rpd_pdf
